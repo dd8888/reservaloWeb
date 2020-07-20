76a9d3828f1739fa95151c816e4248993403f18f
dd8888 <doubled000000@gmail.com>
2020-07-20 20:21:10 +0100

    login completo

--- src/App/LogIn/login.jsx
+++ src/App/LogIn/login.jsx
@@ -49,9 +49,7 @@ const Login = ({ history }) => {
             <div className="row">
                 <div className="col-lg-3 col-md-2"></div>
                 <div className="col-lg-6 col-md-8 login-box">
-                    <div className="col-lg-12 login-key">
-                        <i className="fa fa-key" aria-hidden="true"></i>
-                    </div>
+
                     <div className="col-lg-12 login-title">
                         <img src={logo} className="icon-top" alt="icon" />
                     </div>
